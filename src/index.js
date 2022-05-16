28f0b9dbe3b9e76f1e364c8a16eed74cb02f8e96
HibinoHirotake <101042644+HibinoHirotake@users.noreply.github.com>
2022-05-16 12:15:52 +0900

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,36 @@
+import "./styles.css";
+
+const onClickAdd = () => {
+  ///テキストボックスの値を取得し、初期化する
+  const inputText = document.getElementById("add-text").value;
+  document.getElementById("add-text").value = "";
+
+  //div生成
+  const div = document.createElement("div");
+  div.className = "list-row";
+
+  //liタグ生成
+  const li = document.createElement("li");
+  li.innerText = inputText;
+
+  //button完了タグ生成
+  const completeButton = document.createElement("button");
+  completeButton.innerText = "完了";
+
+  //button削除タグ生成
+  const deleteButton = document.createElement("button");
+  completeButton.innerText = "削除";
+  console.log(deleteButton);
+
+  //divタグの子要素に各要素を設定
+  div.appendChild(li);
+  div.appendChild(completeButton);
+  div.appendChild(deleteButton);
+
+  //未完了リストに追加
+  document.getElementById("incomplete-list").appendChild(div);
+};
+
+document
+  .getElementById("add-button")
+  .addEventListener("click", () => onClickAdd());
